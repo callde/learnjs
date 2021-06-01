44d7e20314b4e680ea39e9ce5fcad005f3a347be
Derek Callaway <decal@sdf.org>
2021-06-01 14:44:14 +0100

    Finish First Problem

--- public/app.js
+++ public/app.js
@@ -6,11 +6,61 @@ var learnjs = {
  
 learnjs.identity = new $.Deferred(); 
 
-learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!'
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+]
 
-  return $('<div class="problem-view">').text(title)
+learnjs.applyObject = function(obj, elem) {
+  for(var key in obj) {
+    elem.find('[data-name="' + key +  '"]').text(obj[key])
+  }
 }
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10)
+  var view = $('.templates .problem-view').clone()
+  var problemData = learnjs.problems[problemNumber - 1]
+  var resultFlash = view.find('.result')
+  
+  function checkAnswer() {
+    var answer = view.find('.answer').val()
+    var test = problemData.code.replace('__', answer) + '; problem();'
+
+    return eval(test)
+  }
+
+  function checkAnswerClick() {
+    if(checkAnswer()) {
+      resultFlash.text('Correct!')
+    } else {
+      resultFlash.text('Incorrect!')
+    }
+
+    return false
+  }
+
+  view.find('.check-btn').click(checkAnswerClick)
+  view.find('.title').text('Problem #' + problemNumber)
+
+  learnjs.applyObject(problemData, view)
+
+  return view
+}
+
+/* learnjs.problemView = function(problemNumber) {
+  var view = $('.templates .problem-view').clone()
+
+  view.find('.title').text('Problem #' + problemNumber)
+
+  return view
+} */
 
 learnjs.showView = function(hash) {
   var routes = {
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -17,10 +17,10 @@ describe('LearnJS', function() {
   })
 
   describe('problem view', function() {
-    it('has a title that includes the problem number', function() {
-      var view = learnjs.problemView('1')
+    var view;
 
-      expect(view.text()).toEqual('Problem #1 Coming soon!')
+    beforeEach(function() {
+      view = learnjs.problemView('1')
     })
 
     it('invokes the router when loaded', function() {
@@ -34,6 +34,21 @@ describe('LearnJS', function() {
       spyOn(learnjs, 'showView')
       $(window).trigger('hashchange')
       expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash)
+    })
+
+    describe('answer section', function() {
+      it('can check a correct answer by hitting a button', function() {
+        view.find('.answer').val('true')
+        view.find('.check-btn').click()
+
+        expect(view.find('.result').text()).toEqual('Correct!')
+      })
+
+      it('rejects an incorrect answer', function() {
+        view.find('.answer').val('false')
+        view.find('.check-btn').click()
+        expect(view.find('.result').text()).toEqual('Incorrect!')
+      })
     })
   })
 })
