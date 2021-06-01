d3fcae45728ceaf208e0513388e9b4238200b82a
Derek Callaway <decal@sdf.org>
2021-06-01 13:50:45 +0100

    Routing Views With Hash Events

--- public/app.js
+++ public/app.js
@@ -6,8 +6,29 @@ var learnjs = {
  
 learnjs.identity = new $.Deferred(); 
 
-learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Coming soon!');
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!'
 
-  $('.view-container').empty().append(problemView);
+  return $('<div class="problem-view">').text(title)
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem': learnjs.problemView
+  }
+
+  var hashParts = hash.split('-')
+  var viewFn = routes[hashParts[0]]
+
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]))
+  }
+}
+
+learnjs.appOnReady = () => {
+  window.onhashchange = () =>  {
+    learnjs.showView(window.location.hash)
+  }
+
+  learnjs.showView(window.location.hash)
 }
